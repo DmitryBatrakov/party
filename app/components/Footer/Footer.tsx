9669f3ea203e359cb61b1656ffcb09958624a666
Dmitry Batrakov <beowoolf61@gmail.com>
2025-06-02 16:25:48 +0300

    second commit

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="h-24 bg-rose-300 w-full flex items-center justify-around p-2">
+    <footer className="h-16 bg-rose-300 w-full flex items-center justify-around p-2">
       <p className="text-white text-sm">© 2025 Девичник Катюши 💕</p>
       <button className="bg-white rounded-lg py-2 px-4 text-rose-300">
         Click
