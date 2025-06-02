0b8b30aca030ca2ecdad72a8bad3c3cb38c162a1
Dmitry Batrakov <beowoolf61@gmail.com>
2025-06-02 17:56:07 +0300

    5 commit

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,19 +1,6 @@
 export default function Footer() {
   return (
     <footer className=" bg-rose-300 w-full flex flex-col gap-4 items-center justify-between p-2">
-      <div className="w-full h-auto flex flex-col justify-between items-center gap-2">
-        <span className="text-sm">
-          Тут можно загрузить и скачать фотографии с девичника
-        </span>
-        <a
-          className="px-2 py-1 bg-white rounded-lg text-rose-300 text-sm"
-          target="_blank"
-          href="https://drive.google.com/drive/folders/1gY-VZMZ0x9C2gXeuBuLKwuHztxFakCDD"
-          rel="noopener noreferrer"
-        >
-          Открыть папку
-        </a>
-      </div>
       <div>
         <span className="text-white text-sm">© 2025 Девичник Катюши 💕</span>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import CardSlider from "./components/CardSlider/CardSlider";
-
+import Link from "./components/Link/Link";
 
 export default function HomePage() {
   return (
-    <div className="p-4">
-      <CardSlider />      
+    <div className="p-4 flex flex-col gap-4">
+      <CardSlider />
+      <Link />      
     </div>
   );
 }
